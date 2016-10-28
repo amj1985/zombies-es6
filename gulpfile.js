8bad98aa3d3b81b82f1b9b3aa12f9f8a1c05c91d
Adrià Manzano Juncosa <Adri Manzano Juncosa>
2016-10-28 13:08:28 +0200

    (Dirty) A.M : Setting up configuration

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-var gulp  = require('gulp'),
+var gulp  = require('gulp');
 var gutil = require('gulp-util');
 var webpack = require('webpack');
 var WebpackDevServer = require('webpack-dev-server');
@@ -8,4 +8,16 @@ var stream = require('webpack-stream');
 // create a default task and just log a message
 gulp.task('default', function() {
   return gutil.log('Gulp is running!')
+});
+gulp.task("webpack-dev-server", function(callback) {
+    // Start a webpack-dev-server
+    var compiler = webpack({
+        // configuration
+    });
+
+    new WebpackDevServer(compiler, {
+    }).listen(8080, "localhost", function(err) {
+        if(err) throw new gutil.PluginError("webpack-dev-server", err);
+        gutil.log("[webpack-dev-server]", "http://localhost:8080/webpack-dev-server/index.html");
+    });
 });
