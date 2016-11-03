240d96a97bbff476e42b5f2f1b40c451e3c5e21f
Adrià Manzano Juncosa <Adri Manzano Juncosa>
2016-11-03 11:54:52 +0100

    Startup project already done

---
+++ src/common/utils.js
@@ -0,0 +1,10 @@
+/**
+ * A.M: This class contains the utils that we will use in this tutorial game.
+ * @class
+ */
+export class Utils {
+    static doSomething(){
+
+    }
+    static getSomething()
+}
