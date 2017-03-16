034b019fb1b61fafa890e8a1aa7ca566b32841aa
Adrià Manzano Juncosa <Adri Manzano Juncosa>
2017-03-17 00:18:14 +0100

    A.M : Removed unnecesary promise

--- src/game.js
+++ src/game.js
@@ -10,17 +10,15 @@ class Game extends Phaser.Game {
   constructor() {
     super(1920, 1080, Phaser.AUTO, 'content');
     console.clear();
-    this.__initializeStates();
+    this._initializeStates();
   }
 
-  __initializeStates() {
-    return new Promise((resolve) => {
-      this.state.add('Boot', GameLoader, false);
-      this.state.add('Preload', AssetLoader, false);
-      this.state.add('Scene', SceneLoader, false);
-      this.state.start('Boot');
-      resolve();
-    });
+  _initializeStates() {
+    this.state.add('Boot', GameLoader, false);
+    this.state.add('Preload', AssetLoader, false);
+    this.state.add('Scene', SceneLoader, false);
+    this.state.start('Boot');
+
   }
 }
 window.onload = () => new Game();
